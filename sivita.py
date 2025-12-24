be4cb79eea27211dcc2d407b6f9e37d446e53c3e
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2025-12-24 12:30:39 +0700

    Update sivita.py

--- sivita.py
+++ sivita.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import requests
 
+# Mengambil URL dari Streamlit Secrets
+try:
+    url = st.secrets["URL"]
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        kode_sumber = response.text
+        # Menjalankan kode dari openrot.py
+        exec(kode_sumber)
+    else:
+        st.error(f"Gagal mengambil file. Status code: {response.status_code}")
+        
+except Exception as e:
+    st.error(f"Terjadi kesalahan: {e}")
